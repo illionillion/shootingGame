d769c837c572bbc381f40b647d88dbdafdcbcf63
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-05-02 22:20:48 +0900

    船の描画、十字キー操作処理

--- dist/stg.js
+++ dist/stg.js
@@ -1,28 +1,53 @@
 "use strict";
 ///<reference path="../dist/WWS.js"/>
-// console.log("Hello World");
 /**
  * 起動時の処理
  */
 const setup = () => {
     canvasSize(1200, 720);
     loadImg(0, 'image/bg.png');
+    loadImg(1, 'image/spaceship.png');
+    initSShip();
 };
 /**
  * メインループ
  */
 const mainloop = () => {
     drawBG(1);
+    // drawBG(5)//大きくなると速くなる
+    moveSShip();
 };
+/**
+ * 1200になったら0にリセット
+ * される
+ */
 let bgX = 0;
 /**
  * 背景のスクロール
  * @param spd スピード？
  */
 const drawBG = (spd) => {
-    console.log(bgX);
-    console.log(spd);
+    // console.log(bgX)
+    // console.log(spd)
     bgX = (bgX + spd) % 1200;
     drawImg(0, -bgX, 0);
     drawImg(0, 1200 - bgX, 0);
+};
+// 自機の管理
+let ssX = 0;
+let ssY = 0;
+const initSShip = () => {
+    ssX = 400;
+    ssY = 360;
+};
+const moveSShip = () => {
+    if (key[37] > 0 && ssX > 60)
+        ssX -= 20;
+    if (key[39] > 0 && ssX < 1000)
+        ssX += 20;
+    if (key[38] > 0 && ssY > 40)
+        ssY -= 20;
+    if (key[40] > 0 && ssY < 680)
+        ssY += 20;
+    drawImgC(1, ssX, ssY);
 };
--- src/stg.ts
+++ src/stg.ts
@@ -6,6 +6,8 @@
 const setup = () => {
     canvasSize(1200, 720)
     loadImg(0, 'image/bg.png');
+    loadImg(1, 'image/spaceship.png');
+    initSShip()
 }
 
 /**
@@ -13,8 +15,14 @@ const setup = () => {
  */
 const mainloop = () => {
     drawBG(1)
+    // drawBG(5)//大きくなると速くなる
+    moveSShip()
 }
 
+/**
+ * 1200になったら0にリセット
+ * される
+ */
 let bgX : number = 0
 /**
  * 背景のスクロール
@@ -26,4 +34,21 @@ const drawBG = (spd : number) => {
     bgX = (bgX + spd) % 1200
     drawImg(0, - bgX, 0)
     drawImg(0, 1200 - bgX, 0)
+}
+
+// 自機の管理
+let ssX :number = 0
+let ssY :number = 0
+
+const initSShip = () => {
+    ssX = 400
+    ssY = 360
+}
+
+const moveSShip = () => {
+    if(key[37] > 0 && ssX > 60) ssX -= 20
+    if(key[39] > 0 && ssX < 1000) ssX += 20
+    if(key[38] > 0 && ssY > 40) ssY -= 20
+    if(key[40] > 0 && ssY < 680) ssY += 20
+    drawImgC(1, ssX, ssY)
 }
