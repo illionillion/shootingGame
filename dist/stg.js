85cec48cd0d3c4e10f39219d3e2175569dcc7497
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-05-04 22:04:58 +0900

    弾を一個だけ打てるようになった

--- dist/stg.js
+++ dist/stg.js
@@ -7,6 +7,7 @@ const setup = () => {
     canvasSize(1200, 720);
     loadImg(0, 'image/bg.png');
     loadImg(1, 'image/spaceship.png');
+    loadImg(2, 'image/missile.png');
     initSShip();
 };
 /**
@@ -16,6 +17,7 @@ const mainloop = () => {
     drawBG(1);
     // drawBG(5)//大きくなると速くなる
     moveSShip();
+    moveMissile();
 };
 /**
  * 1200になったら0にリセット
@@ -36,10 +38,16 @@ const drawBG = (spd) => {
 // 自機の管理
 let ssX = 0;
 let ssY = 0;
+/**
+ * 船の生成
+ */
 const initSShip = () => {
     ssX = 400;
     ssY = 360;
 };
+/**
+ * 船の動作
+ */
 const moveSShip = () => {
     if (key[37] > 0 && ssX > 60)
         ssX -= 20;
@@ -49,5 +57,38 @@ const moveSShip = () => {
         ssY -= 20;
     if (key[40] > 0 && ssY < 680)
         ssY += 20;
+    if (key[32] === 1)
+        setMissile(ssX + 40, ssY, 40, 0);
     drawImgC(1, ssX, ssY);
+};
+//自機が打つ弾の管理
+let mslX, mslY, mslXp, mslYp;
+let mslF = false;
+/**
+ * ミサイルのセット
+ * @param x mslX
+ * @param y mslY
+ * @param xp mslXp
+ * @param yp mslYp
+ */
+const setMissile = (x, y, xp, yp) => {
+    if (!mslF) {
+        mslX = x;
+        mslY = y;
+        mslXp = xp;
+        mslYp = yp;
+        mslF = true;
+    }
+};
+/**
+ * ミサイルの動き
+ */
+const moveMissile = () => {
+    if (mslF) {
+        mslX += mslXp;
+        mslY += mslYp;
+        drawImgC(2, mslX, mslY);
+        if (mslX > 1200)
+            mslF = false;
+    }
 };
--- src/stg.ts
+++ src/stg.ts
@@ -5,8 +5,9 @@
  */
 const setup = () => {
     canvasSize(1200, 720)
-    loadImg(0, 'image/bg.png');
-    loadImg(1, 'image/spaceship.png');
+    loadImg(0, 'image/bg.png')
+    loadImg(1, 'image/spaceship.png')
+    loadImg(2, 'image/missile.png')
     initSShip()
 }
 
@@ -17,18 +18,19 @@ const mainloop = () => {
     drawBG(1)
     // drawBG(5)//大きくなると速くなる
     moveSShip()
+    moveMissile()
 }
 
 /**
  * 1200になったら0にリセット
  * される
  */
-let bgX : number = 0
+let bgX :number = 0
 /**
  * 背景のスクロール
  * @param spd スピード？
  */
-const drawBG = (spd : number) => {
+const drawBG = (spd :number) => {
     // console.log(bgX)
     // console.log(spd)
     bgX = (bgX + spd) % 1200
@@ -40,15 +42,56 @@ const drawBG = (spd : number) => {
 let ssX :number = 0
 let ssY :number = 0
 
+/**
+ * 船の生成
+ */
 const initSShip = () => {
     ssX = 400
     ssY = 360
 }
 
+/**
+ * 船の動作
+ */
 const moveSShip = () => {
     if(key[37] > 0 && ssX > 60) ssX -= 20
     if(key[39] > 0 && ssX < 1000) ssX += 20
     if(key[38] > 0 && ssY > 40) ssY -= 20
     if(key[40] > 0 && ssY < 680) ssY += 20
+    if(key[32] === 1) setMissile(ssX + 40, ssY, 40, 0)
     drawImgC(1, ssX, ssY)
+}
+
+//自機が打つ弾の管理
+let mslX :number, mslY :number, mslXp :number, mslYp :number
+let mslF :boolean = false
+
+/**
+ * ミサイルのセット
+ * @param x mslX
+ * @param y mslY
+ * @param xp mslXp
+ * @param yp mslYp
+ */
+const setMissile = (x :number, y :number, xp :number, yp :number) => {
+    
+    if (!mslF) {
+        mslX = x
+        mslY = y
+        mslXp = xp
+        mslYp = yp
+        mslF = true
+    }
+}
+
+/**
+ * ミサイルの動き
+ */
+const moveMissile = () => {
+    if (mslF) {
+        mslX += mslXp
+        mslY += mslYp
+        drawImgC(2, mslX, mslY)
+        if(mslX > 1200) mslF = false
+    }
 }
